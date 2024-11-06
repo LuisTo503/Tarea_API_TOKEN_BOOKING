f2b26e3965531d0365bfc01fd4497b4dc4b04e32
Carlos Flores <carlosflores3108@hotmail.com>
2024-11-06 00:29:49 -0600

    Agregando funcionalidad para crear nuevas reservaciones

--- src/components/Reservations.jsx
+++ src/components/Reservations.jsx
@@ -4,13 +4,18 @@ import { getBookings } from '../services/bookingServices'
 import { Link } from 'react-router-dom';
 import { Calendar, dayjsLocalizer } from 'react-big-calendar'
 import "react-big-calendar/lib/css/react-big-calendar.css"
+import { useForm } from 'react-hook-form';
 import dayjs from 'dayjs'
+import axios from 'axios'
 
 export default function Bookings() {
     const [bookings, setBookings] = useState([])
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
     //estado para verificar si el usuario esta autenticado
     const [isAuthenticated, setIsAuthenticated] = useState(false)
+    //funciones para el manejo de popup
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { register, handleSubmit } = useForm();
     //inicializando calendario con formato dayJS
     const localizer = dayjsLocalizer(dayjs)
 
@@ -49,6 +54,35 @@ export default function Bookings() {
         return { style: { backgroundColor, color, border: `1px solid ${borderColor}` } };
     };
 
+    const saveBooking = async (data) => {
+        const token = sessionStorage.getItem('token_bookings');
+        try {
+            const response = await axios.post(
+                "https://apibookingsaccomodations-production.up.railway.app/api/V1/booking",
+                {
+                    booking: data.booking,
+                    check_in_date: data.check_in_date,
+                    check_out_date: data.check_out_date,
+                    total_amount: data.total_amount,
+                    accomodation_id: data.accomodation_id ,
+                    user_id: data.user_id
+                },
+                {
+                    headers: {
+                        'Authorization': `Bearer ${token}`,
+                        'Content-Type': 'application/json'
+                    }
+                }
+            );
+            if (response.status === 201) {
+                fetchData();
+                setIsModalOpen(false);
+            }
+        } catch (error) {
+            console.error("Error al guardar el alojamiento:", error.response?.data || error.message);
+        }
+    };
+
     useEffect(() => {
         //validamos si el token existe
         const session_token = sessionStorage.getItem('token_bookings');
@@ -65,6 +99,36 @@ export default function Bookings() {
     const handleOutsideClick = (event) => {
         if (isSidebarOpen && !event.target.closest('.sidebar')) {
             setIsSidebarOpen(false);
+        }
+    };
+
+    const handelSelectEvent = (event) => {
+
+    };
+
+    const editar = async (id, initialData) => {
+        const { value: formValues } = await Swal.fire({
+            title: 'Editar Alojamiento',
+            html:
+                `Nombre:<input id="name" class="swal2-input" placeholder="Nombre" value="${initialData.name}">` +
+                `Direccion:<input id="address" class="swal2-input" placeholder="Dirección" value="${initialData.address}">` +
+                `Descripcion:<input id="description" class="swal2-input" placeholder="Descripción" value="${initialData.description}">`,
+            focusConfirm: false,
+            preConfirm: () => ({
+                name: document.getElementById('name').value,
+                address: document.getElementById('address').value,
+                description: document.getElementById('description').value
+            })
+        });
+
+        if (formValues) {
+            try {
+                await updateAccomodation(id, formValues);
+                Swal.fire('Actualizado', 'El alojamiento ha sido actualizado exitosamente', 'success');
+                fetchData();
+            } catch (error) {
+                Swal.fire('Error', 'Hubo un problema al actualizar el alojamiento', 'error');
+            }
         }
     };
 
@@ -144,6 +208,89 @@ export default function Bookings() {
                     </>
                 ) : <h2>No estas autorizado, inicia sesion</h2>
             }
+            {/* Modal para agregar nuevo alojamiento */}
+            {isModalOpen && (
+                    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
+                        <div style={{ maxWidth: '400px', margin: '0 auto', padding: '20px', border: '1px solid #ccc', borderRadius: '8px', background: 'white', color: 'black' }}>
+                            <h1 style={{ textAlign: 'center' }}>Guardar una Reservación !</h1>
+                            <form onSubmit={handleSubmit(saveBooking)}>
+                                <div style={{ marginBottom: '15px' }}>
+                                    <label htmlFor="booking">Booking: </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Ingresar clave de Booking"
+                                        {...register('booking', { required: true })}
+                                        style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+                                    />
+                                </div>
+
+                                <div style={{ marginBottom: '15px' }}>
+                                    <label htmlFor="check_in_date">Check In Date: </label>
+                                    <input
+                                        placeholder="Fecha de Ingreso"
+                                        {...register('check_in_date', { required: true })}
+                                        style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+                                    />
+                                </div>
+
+                                <div style={{ marginBottom: '15px' }}>
+                                    <label htmlFor="check_out_date">Check Out Date: </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Fecha de Salida"
+                                        {...register('check_out_date', { required: true })}
+                                        style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+                                    />
+                                </div>
+
+                                <div style={{ marginBottom: '15px' }}>
+                                    <label htmlFor="total_amount">Total Amount: </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Ingrese Costo Total"
+                                        {...register('total_amount', { required: true })}
+                                        style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+                                    />
+                                </div>
+
+                                <div style={{ marginBottom: '15px' }}>
+                                    <label htmlFor="accomodation_id">Accomodation ID: </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Ingrese Accomodation ID"
+                                        {...register('accomodation_id', { required: true })}
+                                        style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+                                    />
+                                </div>
+
+                                <div style={{ marginBottom: '15px' }}>
+                                    <label htmlFor="user_id">User ID: </label>
+                                    <input
+                                        type="text"
+                                        placeholder="Ingrese ID de Usuario"
+                                        {...register('user_id', { required: true })}
+                                        style={{ width: '100%', padding: '8px', marginTop: '5px' }}
+                                    />
+                                </div>
+
+                                <button
+                                    type="submit"
+                                    style={{ width: '45%', padding: '10px', backgroundColor: '#007bff', color: '#fff', border: 'none', borderRadius: '4px', marginBottom: '10px', marginRight: '10px'}}
+                                >
+                                    Guardar
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={() => setIsModalOpen(false)}
+                                    style={{ width: '45%', padding: '10px', backgroundColor: '#dc3545', color: '#fff', border: 'none', borderRadius: '4px' }}
+                                >
+                                    Cancelar
+                                </button>
+                            </form>
+                        </div>
+                    </div>
+                )}
+
             </main>
         </div>
     )
